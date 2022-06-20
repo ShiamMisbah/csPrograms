52448fd7995283bceca89b4729bfd2f01455ece6
ShiamMisbah <shiam.misbah42@gmail.com>
2022-06-20 16:37:12 +0600

    2 folder added

---
+++ EasyProjects/EasyProjects/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EasyProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3,5,7,2,5 };
+            int target = 9;
+
+            TwoSum(ints, target);
+        }
+        static void TwoSum(int[] nums, int target)
+        {
+            int[] li = new int[2];
+            for (int con = 0; con < nums.Length; con++)
+            {
+                for (int i = con + 1; i < nums.Length; i++)
+                {
+                    if (nums[con] + nums[i] == target)
+                    {
+                        li[0] = con;
+                        li[1] = i;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("[{0},{1}]",li[0],li[1]);
+        }
+    }
+}
---
+++ PDFGenerator/PDFGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        //New Document
+        PdfDocument document = new PdfDocument();
+        Console.WriteLine("Done1");
+        //New page
+        PdfPage page = document.AddPage();
+        Console.WriteLine("Done2");
+        //graphics and stuffs
+        XGraphics gfx = XGraphics.FromPdfPage(page);
+        Console.WriteLine("Done3");
+        //font
+        XFont font = new XFont("Arial", 20);
+        Console.WriteLine("Done4");
+        //writing text
+        gfx.DrawString("My name is Turjo", font, XBrushes.Blue,
+            new XRect(0, 0, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+        Console.WriteLine("Done5");
+        document.Save("C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\PDFGenerator\\PDF\\testPdf.pdf");
+        Console.WriteLine("Done6");
+    }
+
+}
+
+
+
