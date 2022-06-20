7a20d7036e9bd7f40cbabf1e13b135288afcda45
ShiamMisbah <shiam.misbah42@gmail.com>
2022-06-20 16:58:53 +0600

    small update

--- PDFGenerator/PDFGenerator/Program.cs
+++ PDFGenerator/PDFGenerator/Program.cs
@@ -22,10 +22,13 @@ class Program
         XFont font = new XFont("Arial", 20);
         Console.WriteLine("Done4");
         //writing text
-        gfx.DrawString("My name is Turjo", font, XBrushes.Blue,
-            new XRect(0, 0, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+        gfx.DrawString("My name is Tusdgagrjo", font, XBrushes.Blue,
+            new XRect(100, 300, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+
+        gfx.DrawString("My name is Not Turjo", font, XBrushes.Red,
+            new XPoint(100, 500));
         Console.WriteLine("Done5");
-        document.Save("C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\PDFGenerator\\PDF\\testPdf.pdf");
+        document.Save("C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\csPrograms\\PDFGenerator\\PDF\\testPdf.pdf");
         Console.WriteLine("Done6");
     }
 
