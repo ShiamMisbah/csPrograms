885a52f4b0bb42e61c9da53937f4f10ee37f1106
ShiamMisbah <shiam.misbah42@gmail.com>
2022-06-22 15:25:07 +0600

    add image in pdf

--- PDFGenerator/PDFGenerator/Program.cs
+++ PDFGenerator/PDFGenerator/Program.cs
@@ -1,38 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-        //New Document
-        PdfDocument document = new PdfDocument();
-        Console.WriteLine("Done1");
-        //New page
-        PdfPage page = document.AddPage();
-        Console.WriteLine("Done2");
-        //graphics and stuffs
-        XGraphics gfx = XGraphics.FromPdfPage(page);
-        Console.WriteLine("Done3");
-        //font
-        XFont font = new XFont("Arial", 20);
-        Console.WriteLine("Done4");
-        //writing text
-        gfx.DrawString("My name is Tusdgagrjo", font, XBrushes.Blue,
-            new XRect(100, 300, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+//New Document
+PdfDocument document = new PdfDocument();
+Console.WriteLine("Done1");
+//New page
+PdfPage page = document.AddPage();
+Console.WriteLine("Done2");
+//graphics and stuffs
+XGraphics gfx = XGraphics.FromPdfPage(page);
+Console.WriteLine("Done3");
+//font
+XFont font = new XFont("Arial", 20);
+Console.WriteLine("Done4");
+//writing text
+gfx.DrawString("My name is Tusdgagrjo", font, XBrushes.Blue,
+    new XRect(100, 400, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
 
-        gfx.DrawString("My name is Not Turjo", font, XBrushes.Red,
-            new XPoint(100, 500));
-        Console.WriteLine("Done5");
-        document.Save("C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\csPrograms\\PDFGenerator\\PDF\\testPdf.pdf");
-        Console.WriteLine("Done6");
-    }
+gfx.DrawString("My name is Not Haba Haba Turjo", font, XBrushes.Red,
+    new XPoint(100, 400));
+Console.WriteLine("Done5");
 
-}
+
+
+//Image Draw
+
+XImage img = XImage.FromFile("C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\csPrograms\\PDFGenerator\\PDF\\image1.jfif");
+gfx.DrawImage(img, 375, 10);
+Console.WriteLine("Done6");
+document.Save("C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\csPrograms\\PDFGenerator\\PDF\\testPdf.pdf");
+Console.WriteLine("Done7");
 
 
 
