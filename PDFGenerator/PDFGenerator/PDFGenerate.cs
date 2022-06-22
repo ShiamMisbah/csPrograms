ac5cc7639f9eb242e55253794721684e825798bf
ShiamMisbah <shiam.misbah42@gmail.com>
2022-06-22 16:43:48 +0600

    modified

---
+++ PDFGenerator/PDFGenerator/PDFGenerate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace PDFGenerator
+{
+    class PDFGenerate
+    {
+        public static void BarcodeImageGenerator(string BarNum, string SrcDir)
+        {
+            PdfDocument document = new PdfDocument();
+            //New page
+            PdfPage page = document.AddPage();
+            //graphics and stuffs
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            //font
+            XFont font = new XFont("Arial", 20);
+            //writing text
+            gfx.DrawString("My name is Tusdgagrjo", font, XBrushes.Blue,
+                new XRect(50, 100, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+
+            gfx.DrawString("My name is Not Haba Haba Turjo", font, XBrushes.Red,
+                new XPoint(50, 150));
+            XImage img = XImage.FromFile(SrcDir + BarNum + ".png");
+            gfx.DrawImage(img, 400, 650, 200, 125);
+            document.Save(SrcDir + BarNum + ".pdf");
+
+        }
+    }
+}
--- PDFGenerator/PDFGenerator/Program.cs
+++ PDFGenerator/PDFGenerator/Program.cs
@@ -6,39 +6,17 @@ using PdfSharp.Pdf;
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 //generate Random Number
 Random random = new Random();
-////New Document
-//PdfDocument document = new PdfDocument();
-//Console.WriteLine("Done1");
-////New page
-//PdfPage page = document.AddPage();
-//Console.WriteLine("Done2");
-////graphics and stuffs
-//XGraphics gfx = XGraphics.FromPdfPage(page);
-//Console.WriteLine("Done3");
-////font
-//XFont font = new XFont("Arial", 20);
-//Console.WriteLine("Done4");
-////writing text
-//gfx.DrawString("My name is Tusdgagrjo", font, XBrushes.Blue,
-//    new XRect(50, 100, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
-
-//gfx.DrawString("My name is Not Haba Haba Turjo", font, XBrushes.Red,
-//    new XPoint(50, 150));
-//Console.WriteLine("Done5");
 
 string dir = "C:\\Users\\Administrator\\Desktop\\Shiam\\C#\\csPrograms\\PDFGenerator\\PDF\\";
 
-//Image Draw
 
-
-Console.WriteLine("Enter Barcode Number: \n");
 
 for (int i = 0; i<5;i++)
 {
-    string BarNum = Convert.ToString(random.Next(1000));
+    string BarNum = Convert.ToString(random.Next(10000000));
     Console.WriteLine(BarNum);
     PDFGenerator.BarcodeGenerator.GenerateBarcode(BarNum, dir);
-    PDFGenerator.BarcodeGenerator.BarcodeImageGenerator(BarNum, dir);
+    PDFGenerator.PDFGenerate.BarcodeImageGenerator(BarNum, dir);
 
 
 }
