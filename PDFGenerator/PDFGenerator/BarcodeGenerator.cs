65a07b3e67f5eab42893705648ae72cdc164ced7
ShiamMisbah <shiam.misbah42@gmail.com>
2022-06-27 17:03:50 +0600

    font updated

--- PDFGenerator/PDFGenerator/BarcodeGenerator.cs
+++ PDFGenerator/PDFGenerator/BarcodeGenerator.cs
@@ -8,7 +8,6 @@ namespace PDFGenerator
         public static string GenerateBarcode(string BarNum,string dir)
         {
             var barcode = BarcodeWriter.CreateBarcode(BarNum, BarcodeEncoding.Code128);
-            barcode.AddBarcodeValueTextBelowBarcode();
             var newDir = dir + "Barcodes\\" + BarNum + ".png";
             barcode.SaveAsPng(newDir);
             return newDir;
--- PDFGenerator/PDFGenerator/PDFGenerate.cs
+++ PDFGenerator/PDFGenerator/PDFGenerate.cs
@@ -21,38 +21,59 @@ namespace PDFGenerator
             //graphics and stuffs
             XGraphics gfx = XGraphics.FromPdfPage(page);
             //font
-            XFont fontID = new XFont("Arial", 24);
-            XFont fontName = new XFont("Arial", 18);
-            XFont fontPhone = new XFont("Arial", 12);
-            XFont fontEmail = new XFont("Arial", 12);
+            XFont FontID = new XFont("Arial", 24);
+            XFont FontName = new XFont("Arial", 18);
+            XFont Font = new XFont("Arial", 15);
+            XFont BanglaFont = new XFont("Nirmala UI", 12);
+            // Margin Set ups
+            int LeftM = 50;
+            int RightM = 50;
+            int TopM = 150;
+            int BottomM = 150;
+            int BottomInfoStart = 325;
+            int VerticalGap = 30;
+
             //writing text
-            if (IsCandidate)
-            {
-                gfx.DrawString(Convert.ToString(ID), fontID, XBrushes.Green,
-                    new XPoint(50, 50));
-                gfx.DrawString(Convert.ToString(Name), fontName, XBrushes.Green,
-                    new XPoint(150, 50));
-            }
-            else
-            {
-                gfx.DrawString(Convert.ToString(ID), fontID, XBrushes.Red,
-                    new XPoint(50, 50));
-                gfx.DrawString(Convert.ToString(Name), fontName, XBrushes.Red,
-                    new XPoint(150, 50));
-            }
 
 
-            
-            gfx.DrawString(Convert.ToString(Phone), fontPhone, XBrushes.Black,
-                new XPoint(50, 100));
-            gfx.DrawString(Convert.ToString(Email), fontEmail, XBrushes.Black,
-                new XPoint(150, 100));
+            gfx.DrawString("ID: "+ Convert.ToString(ID), FontID, XBrushes.Black,
+                new XPoint(LeftM, 150));
+            gfx.DrawString("Full Name: " + Convert.ToString(Name), FontName, XBrushes.Black,
+                new XPoint(LeftM, 200));
+            gfx.DrawString("পুর নাম: আব্দুর রউফ", new XFont("SolaimanLipi", 15), XBrushes.Black,
+                new XPoint(LeftM, 225));
 
-            
+            gfx.DrawString("Phone Number: "+Convert.ToString(Phone), Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart));
+            gfx.DrawString("Email Address: "+Convert.ToString(Email), Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap));
+            gfx.DrawString("Father's Name: Asfjkrhgg Uddin ", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*2));
+            gfx.DrawString("পিতার নাম: Asfjkrhgg Uddin ", BanglaFont, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*3));
+            gfx.DrawString("Mother's Name: ", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*4));
+            gfx.DrawString("মাতার নাম: ", BanglaFont, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*5));
+            gfx.DrawString("Date of Birth: ", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*6));
+            gfx.DrawString("Address: Dhanmondi, Dhaka", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*7));
+            gfx.DrawString("ঠিকানা: Dhanmondi, Dhaka", BanglaFont, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*8));
+            gfx.DrawString("Parmanent Address: Rangpur, Bangladesh", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*9));
+            gfx.DrawString("Religion: Hindu", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*10));           
+            gfx.DrawString("Nationality: Bangladeshi", Font, XBrushes.Black,
+                new XPoint(LeftM, BottomInfoStart + VerticalGap*11));
 
-            XImage img = XImage.FromFile(BarcodeGenerator.GenerateBarcode(BarNum, SrcDir));
-            gfx.DrawImage(img, 400, 650, 200, 125);
-            document.Save(SrcDir + BarNum + ".pdf");
+            XImage ImgProfile = XImage.FromFile(@"C:\Users\Administrator\Desktop\Shiam\C#\csPrograms\PDFGenerator\PDF\Images\shk_old.png");
+            gfx.DrawImage(ImgProfile, 400, TopM-20,150,175);
+
+            XImage ImgBarcode = XImage.FromFile(BarcodeGenerator.GenerateBarcode(BarNum, SrcDir));
+            gfx.DrawImage(ImgBarcode, 400, 700,150,50);
+            document.Save(SrcDir + ID + ".pdf");
 
         }
     }
