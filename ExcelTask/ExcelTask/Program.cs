1ab01ec9bd7f7b32935cce9143adbe87f1c71135
shiblypwd <shibly.dev.pwd@gmail.com>
2022-07-05 16:36:28 +0600

    Duplicate search from Excel Complete

--- ExcelTask/ExcelTask/Program.cs
+++ ExcelTask/ExcelTask/Program.cs
@@ -7,27 +7,27 @@ watch.Start();
 WorkBook WB = WorkBook.Load(@"C:\Users\Admin\Desktop\Shiam\csPrograms\ExcelTask\DataSet\Recruit_Applicants.xlsx");
 WorkSheet WS = WB.GetWorkSheet("Sheet1");
 Console.WriteLine("Worksheet Read: {0}", watch.ElapsedMilliseconds);
-Console.WriteLine("Enter initial value: ");
-string Initial = "F"+Console.ReadLine();
+string Initial = "F2";
 Console.WriteLine("Enter Final value: ");
 string Final = "F"+Console.ReadLine();
 string CellRange = Initial+":"+Final;
 Console.WriteLine(CellRange);
 var Range = WS.GetRange(CellRange);
 
-HashSet<int> list = new HashSet<int>();
-List<int> li2 = new List<int>();
-
+HashSet<string> list = new HashSet<string>();
+//List<IronXL.Cell> li2 = new List<IronXL.Cell>();
+int DuplicateCount = 0;
 foreach (var cell in Range)
 {
-    if (!list.Add(cell.Int32Value))
+    if (!list.Add(cell.ToString()))
     {
-        Console.WriteLine("The Duplicate are: {0}", cell.Int32Value);
+        Console.WriteLine("The Duplicate are: {0}", cell.ToString());
+        DuplicateCount++;
     }
 }
 
-
-
+Console.WriteLine("Total Duplicates: {0}", DuplicateCount);
+Console.WriteLine("Total Uniques: {0}", list.Count);
 watch.Stop();
 
 Console.WriteLine("Final Execution Time: {0}", watch.ElapsedMilliseconds);
